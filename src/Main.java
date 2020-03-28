bc60f3b091776a6a3be1c4515112eb72784633cb
elenaslavutina <elena.slavutina2013@gmail.com>
2020-03-28 20:38:45 +0300

    init

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+	    int start = 100;
+        int payment = 700;
+        int bonus = payment / 100;
+        System.out.println("Итоговый бонус = " + bonus);
+    }
+}
