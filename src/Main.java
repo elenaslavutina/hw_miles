8a2126d6bb966caf2debca8b50a223b0c74b161f
elenaslavutina <elena.slavutina2013@gmail.com>
2020-03-29 16:13:14 +0300

    change main

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
 
     public static void main(String[] args) {
-	    int price = 11021;
+        int price = 11021;
         System.out.println("Цена билета  = " + price);
-        int mile = price/20;
+        int mile = price / 20;
         System.out.println("Количество миль  = " + mile);
     }
 }
